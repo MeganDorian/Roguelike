dd9f3831a901b1d1be513c1ab45cf3e9c777f445
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-06-10 15:33:06 +0300

    feat: add up mob

---
+++ src/main/java/org/itmo/mse/constants/Specifications.java
@@ -0,0 +1,14 @@
+package org.itmo.mse.constants;
+
+/**
+ * Value for default specifications mob
+ */
+public class Specifications {
+    public static int defaultExperienceCowardlyMob = 3;
+    public static int defaultExperiencePassiveMob = 5;
+    public static int defaultExperienceAggressiveMob = 10;
+    public static int lowerMobDamage = 1;
+    public static int upperMobDamage = 2;
+    public static int lowerMobHealthy = 1;
+    public static int upperMobHealthy = 3;
+}
